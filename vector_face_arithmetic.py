21fead34f61ea032d545fe2b260ace73b19f2477
ashutosh1919 <ashutoshhathidara98@gmail.com>
2020-01-15 06:38:22 +0000

    vector arithmatic

---
+++ vector_face_arithmetic.py
@@ -0,0 +1,64 @@
+# example of loading the generator model and generating images
+from numpy import asarray
+from numpy.random import randn
+from numpy.random import randint
+from keras.models import load_model
+from matplotlib import pyplot
+from numpy import load
+from numpy import mean
+from numpy import vstack
+from numpy import expand_dims
+
+# average list of latent space vectors
+def average_points(points, ix):
+	# convert to zero offset points
+	zero_ix = [i-1 for i in ix]
+	# retrieve required points
+	vectors = points[zero_ix]
+	# average the vectors
+	avg_vector = mean(vectors, axis=0)
+	# combine original and avg vectors
+	all_vectors = vstack((vectors, avg_vector))
+	return all_vectors
+
+# create a plot of generated images
+def plot_generated(examples, rows, cols):
+	# plot images
+	for i in range(rows * cols):
+		# define subplot
+		pyplot.subplot(rows, cols, 1 + i)
+		# turn off axis
+		pyplot.axis('off')
+		# plot raw pixel data
+		pyplot.imshow(examples[i, :, :])
+	pyplot.show()
+
+# load model
+model = load_model('generated_models/generator_model_030.h5')
+# retrieve specific points
+smiling_woman_ix = [29, 87, 100]
+neutral_woman_ix = [4, 19, 70]
+neutral_man_ix = [2, 25, 44]
+# load the saved latent points
+data = load('latent_points.npz')
+points = data['arr_0']
+# average vectors
+smiling_woman = average_points(points, smiling_woman_ix)
+neutral_woman = average_points(points, neutral_woman_ix)
+neutral_man = average_points(points, neutral_man_ix)
+# combine all vectors
+all_vectors = vstack((smiling_woman, neutral_woman, neutral_man))
+# generate images
+images = model.predict(all_vectors)
+# scale pixel values
+images = (images + 1) / 2.0
+plot_generated(images, 3, 4)
+# smiling woman - neutral woman + neutral man = smiling man
+result_vector = smiling_woman[-1] - neutral_woman[-1] + neutral_man[-1]
+# generate image
+result_vector = expand_dims(result_vector, 0)
+result_image = model.predict(result_vector)
+# scale pixel values
+result_image = (result_image + 1) / 2.0
+pyplot.imshow(result_image[0])
+pyplot.savefig('generated_plots/result_of_vector_arithmetic.png')
