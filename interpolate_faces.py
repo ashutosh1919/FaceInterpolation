3d8720023acbb528ea5fb7afd2134978c57a090e
ashutosh1919 <ashutoshhathidara98@gmail.com>
2020-01-15 05:18:16 +0000

    interpolating faces

--- interpolate_faces.py
+++ interpolate_faces.py
@@ -1,7 +1,8 @@
-# example of loading the generator model and generating images
+# example of interpolating between generated faces
 from numpy import asarray
 from numpy.random import randn
 from numpy.random import randint
+from numpy import linspace
 from keras.models import load_model
 from matplotlib import pyplot
 
@@ -13,12 +14,23 @@ def generate_latent_points(latent_dim, n_samples, n_classes=10):
 	z_input = x_input.reshape(n_samples, latent_dim)
 	return z_input
 
+# uniform interpolation between two points in latent space
+def interpolate_points(p1, p2, n_steps=10):
+	# interpolate ratios between the points
+	ratios = linspace(0, 1, num=n_steps)
+	# linear interpolate vectors
+	vectors = list()
+	for ratio in ratios:
+		v = (1.0 - ratio) * p1 + ratio * p2
+		vectors.append(v)
+	return asarray(vectors)
+
 # create a plot of generated images
 def plot_generated(examples, n, filepath):
 	# plot images
-	for i in range(n * n):
+	for i in range(n):
 		# define subplot
-		pyplot.subplot(n, n, 1 + i)
+		pyplot.subplot(1, n, 1 + i)
 		# turn off axis
 		pyplot.axis('off')
 		# plot raw pixel data
@@ -27,11 +39,13 @@ def plot_generated(examples, n, filepath):
 
 # load model
 model = load_model('generated_models/generator_model_030.h5')
+# generate points in latent space
+pts = generate_latent_points(100, 2)
+# interpolate points in latent space
+interpolated = interpolate_points(pts[0], pts[1])
 # generate images
-latent_points = generate_latent_points(100, 25)
-# generate images
-X  = model.predict(latent_points)
+X = model.predict(interpolated)
 # scale from [-1,1] to [0,1]
 X = (X + 1) / 2.0
 # plot the result
-plot_generated(X, 5, 'generated_plots/generated_faces.png')
+plot_generated(X, len(interpolated), 'generated_plots/interpolating_faces.png')
